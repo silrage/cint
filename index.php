11400c5b323022c9125dfc4d5e1965aff9a31659
SilRage <silrage23@mail.ru>
2016-05-06 17:58:44 +0300

    update

--- index.php
+++ index.php
@@ -18,6 +18,45 @@
         color: #777;
         padding: 20px;
       }
+      a {
+        cursor: pointer;
+      }
+      .gallery {}
+      .gallery .image-item {
+        float: left;
+        position: relative;
+      }
+      .gallery .image-item .mouse-in {
+        position: absolute;
+        background: rgba(0,0,0, .3);
+        width: 100%;
+        height: 100%;
+        opacity: 0;
+      }
+      .gallery .image-item .mouse-in i {
+        position: absolute;
+        margin: auto;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        width: 20px;
+        height: 20px;
+        color: #fff;
+        font-size: 20px;
+      }
+      .gallery .image-item:hover .mouse-in {
+        opacity: 1;
+      }
+      .gallery .image-item .counts {
+        background: rgba(0,0,0, .6);
+        position: absolute;
+        top: 0;
+      }
+      .gallery .image-item .counts > div {
+        float: left;
+        color: #fff;
+      }
     </style>
 	</head>
 	<body>
@@ -40,7 +79,7 @@
           <!-- Instagram API -->
           <?include_once $currentPlugin['config'];?>
           <h3>Instagram</h3>
-          <a href="https://api.instagram.com/oauth/authorize/?client_id=cb2e702fde06407da2bfeb9ffdb6618f&redirect_uri=http://cint.dev&response_type=token&scope=basic+public_content">Authorize</a>
+          <a href="https://api.instagram.com/oauth/authorize/?client_id=cb2e702fde06407da2bfeb9ffdb6618f&redirect_uri=http://cint.dev&response_type=token&scope=basic+public_content+follower_list+relationships+likes ">Authorize</a>
         </div>
         <!-- Library -->
         <script type="text/javascript" src="<?=$currentPlugin['libs'];?>"></script>
@@ -51,12 +90,52 @@
     <div class="load" ng-if="auth.insta" ng-controller="objects">
       <div class="insta">
         <a class="" ng-click="view()">
-          refresh
+          <i class="glyphicon glyphicon-refresh"></i> refresh
         </a>
-        <div class="profile">
-          <img ng-src="{{profile.insta.profile_picture}}" />
-          <b>{{profile.insta.username}}</b>
-          {{profile.insta.full_name}}
+        <a class="" ng-click="exit()">
+          <i class="glyphicon glyphicon-log-out"></i> exit
+        </a>
+        <div class="profile row">
+          <div class="col-md-6">
+            <img ng-src="{{profile.insta.profile_picture}}" />
+          </div>
+          <div class="col-md-6">
+            <h3>{{profile.insta.username}}</h3>
+            <p>
+              <b>{{profile.insta.full_name}}</b>
+            </p>
+            <span>Публикаций {{profile.insta.counts.media}}</span>
+            <span>Подписчиков {{profile.insta.counts.followed_by}}</span>
+            <span>Подписки: {{profile.insta.counts.follows}}</span>
+          </div>
+        </div>
+
+        <div class="subscribers">
+          <ul>
+            <li ng-repeat="followed_by in profile.insta.followed_by | limitTo: followed_by.countFollowers">
+              {{followed_by.name}}
+            </li>
+          </ul>
+        </div>
+
+        <div class="gallery">
+          <h3>Популярное (более 7 лайков)</h3>
+          <div class="image-item" ng-repeat="image in gallery | limitTo: gallery.countImages">
+            <a href="{{image.link}}" target="_blank">
+              <div class="mouse-in">
+                <i class="glyphicon glyphicon-new-window"></i>
+              </div>
+              <img ng-src="{{image.images[gallery.size].url}}" />
+            </a>
+            <div class="counts">
+              <div class="likes">
+                <i class="glyphicon glyphicon-heart"></i>{{image.likes.count}}
+              </div>
+              <div class="comments">
+                <i class="glyphicon glyphicon-comment">{{image.comments.count}}</i>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
